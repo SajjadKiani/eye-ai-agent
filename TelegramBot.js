1c80ced0cc01ef364f6d8bd2d337efe852d0bdf0
Saji <skm.kiani@gmail.com>
2024-12-31 16:57:18 +0330

    initial telegram bot

---
+++ TelegramBot.js
@@ -0,0 +1,34 @@
+import TelegramBot from "node-telegram-bot-api";
+
+
+class TelBot {
+    constructor(token, desChannelId, srcChannelId) {
+        this.bot = new TelegramBot(token, {polling: true})
+        this.token = token;
+        this.desChannelId = desChannelId;
+        this.srcChannelId = srcChannelId;
+    }
+
+    async sendMessage(text) {
+        try {
+            await this.bot.sendMessage(this.desChannelId, text);
+            console.log('Message sent successfully');
+        } catch (error) {
+            console.error('Error sending message:', error);
+        }
+    }
+
+    async getChannelText() {
+        try {
+            this.bot.on('channel_post', async (msg) => {
+                if (msg.chat.id === this.srcChannelId) {
+                    console.log('Received message from source channel:', msg.text);
+                }
+            });
+        } catch (error) {
+            console.error('Error getting channel text:', error);
+        }
+    }
+}
+
+export default TelBot;
